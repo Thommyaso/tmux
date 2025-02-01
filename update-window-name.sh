cc3093fc70e932e148c214f2b87c434d9d8610a1
thomas <tomgaek@gmail.com>
2025-02-01 12:16:03 +0000

    Bugfix: Prevent window 0 from getting its name changed

--- update-window-name.sh
+++ update-window-name.sh
@@ -3,7 +3,7 @@
 SESSION="$1"
 WINDOW="$2"
 
-if [[ "$WINDOW" != "0" ]]; then
+if [[ "$WINDOW" -ne  0 ]]; then
     DIRECTORY=$( basename $(tmux display-message -p -t "${SESSION}:${WINDOW}.1" "#{pane_current_path}"))
 
     tmux rename-window -t "$SESSION":"$WINDOW" "$DIRECTORY"
